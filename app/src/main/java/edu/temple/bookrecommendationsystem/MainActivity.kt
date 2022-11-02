defabcde1309e784674ff6c4d5d3609c6e433d82
Harrison Fedor <fedor@temple.edu>
2022-11-02 13:56:50 -0400

    Removed template code and populated container2
    - Removed excess code in blank Fragments
    - Set constraints for container2
    - Populated container2 with ToolbarFragment

--- app/src/main/java/edu/temple/bookrecommendationsystem/MainActivity.kt
+++ app/src/main/java/edu/temple/bookrecommendationsystem/MainActivity.kt
@@ -7,7 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.container2, ToolbarFragment())
+            .commit()
         //populate fragment containers in recyclerview
     }
 }
