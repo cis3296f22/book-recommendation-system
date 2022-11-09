9a7bef3dbe04f752e7dd5d5b2d77278db38a7f73
Harrison Fedor <fedor@temple.edu>
2022-11-09 14:34:19 -0500

    Implemented Layout and Toolbar Functionality

--- app/src/main/java/edu/temple/bookrecommendationsystem/MainActivity.kt
+++ app/src/main/java/edu/temple/bookrecommendationsystem/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.bookrecommendationsystem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,10 +10,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         supportFragmentManager
             .beginTransaction()
+            .add(R.id.container1, RecommendationFragment())
             .add(R.id.container2, ToolbarFragment())
             .commit()
-
-
         //populate fragment containers in recyclerview
     }
 }
