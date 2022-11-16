0d196c565db696a8005801457a4b8bc6f1abde20
Harrison Fedor <fedor@temple.edu>
2022-11-16 14:34:42 -0500

    Created Book data class & Rec Fragment cycling

--- app/src/main/java/edu/temple/bookrecommendationsystem/SearchFragment.kt
+++ app/src/main/java/edu/temple/bookrecommendationsystem/SearchFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 
 /**
  * A simple [Fragment] subclass.
@@ -14,14 +15,15 @@ import android.view.ViewGroup
 class SearchFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        val rootview = inflater.inflate(R.layout.fragment_search, container, false)
+        return rootview
     }
 
 }
