e4880da61ab4740be922cd18ee2f8e330ebcc628
Harrison Fedor <fedor@temple.edu>
2022-11-27 12:18:50 -0500

    Misc. mostly aesthetic changes

--- app/src/main/java/edu/temple/bookrecommendationsystem/BookListsFragment.kt
+++ app/src/main/java/edu/temple/bookrecommendationsystem/BookListsFragment.kt
@@ -23,4 +23,8 @@ class BookListsFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_book_lists, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
