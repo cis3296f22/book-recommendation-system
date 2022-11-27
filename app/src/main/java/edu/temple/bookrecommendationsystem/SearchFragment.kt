ec6eb10b648906a95e2a626c1d17b57a45cd357a
Harrison Fedor <fedor@temple.edu>
2022-11-27 12:17:27 -0500

    Implemented SearchAdapter

--- app/src/main/java/edu/temple/bookrecommendationsystem/SearchFragment.kt
+++ app/src/main/java/edu/temple/bookrecommendationsystem/SearchFragment.kt
@@ -5,9 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.Toast
+import android.widget.*
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 /*
 A fragment class containing a search bar that will send a query to our database and return the top
@@ -27,10 +28,27 @@ class SearchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = requireView().findViewById<RecyclerView>(R.id.search_results_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this.context)
+        recyclerView.adapter = SearchAdapter(Application().dummyBooks) {
+            val fragment = BookDetailsFragment()
+            val bundle = Bundle()
+            bundle.putString("title", it.title)
+            bundle.putString("author", it.author)
+            bundle.putInt("cover", it.coverURL)
+            fragment.arguments = bundle
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.container1, fragment)
+                .addToBackStack(null)
+                .commit()
+        }
+
         requireView().findViewById<ImageButton>(R.id.search_button).setOnClickListener {
             val query = requireView().findViewById<EditText>(R.id.search_edit_text).text
-            //call search w query
-            //populate recyclerview with book results
+            //TODO: make so user cannot put a new line in the search text box
+            //TODO: call search w query
+            //TODO: populate recyclerview with book results (color books first, then real)
             Toast.makeText(this.context, query, Toast.LENGTH_LONG).show()
         }
     }
